6db9e21d7b3c4899aa1c18901e015dc2fc488f90
ImHeroesKiller <148073026+ImHeroesKiller@users.noreply.github.com>
2024-09-07 18:17:11 +0700

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+// Initialize the BPMN Modeler
+var bpmnModeler = new BpmnJS({
+    container: '#canvas',
+    keyboard: {
+        bindTo: window
+    }
+});
+
+// Load an empty diagram for editing
+var newDiagram = `<?xml version="1.0" encoding="UTF-8"?>
+<bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
+                  xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
+                  xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"
+                  xmlns:di="http://www.omg.org/spec/DD/20100524/DI"
+                  id="Definitions_1" targetNamespace="http://bpmn.io/schema/bpmn">
+  <bpmn:process id="Process_1" isExecutable="false">
+    <bpmn:startEvent id="StartEvent_1" />
+  </bpmn:process>
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
+      <bpmndi:BPMNShape id="StartEvent_1_di" bpmnElement="StartEvent_1">
+        <dc:Bounds x="173" y="102" width="36" height="36" />
+      </bpmndi:BPMNShape>
+    </bpmndi:BPMNPlane>
+  </bpmndi:BPMNDiagram>
+</bpmn:definitions>`;
+
+// Import the diagram into the modeler
+bpmnModeler.importXML(newDiagram, function(err) {
+    if (err) {
+        console.error('Error importing BPMN diagram:', err);
+    } else {
+        console.log('BPMN diagram imported successfully!');
+        bpmnModeler.get('canvas').zoom('fit-viewport');
+    }
+});
